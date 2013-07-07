e7c171e0b894d37bc6c744228c34aae2f3c21ed8
Lev Ananikyan <lev.ananikyan@gmail.com>
2013-07-07 15:59:07 +0400

    Installation

---
+++ upload/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.local.dev/');
+define('HTTP_IMAGE', 'http://opencart.local.dev/image/');
+define('HTTP_ADMIN', 'http://opencart.local.dev/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.local.dev/');
+define('HTTPS_IMAGE', 'http://opencart.local.dev/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\opencart\upload/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\opencart\upload/system/');
+define('DIR_DATABASE', 'C:\wamp\www\opencart\upload/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\opencart\upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\opencart\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\opencart\upload/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\opencart\upload/image/');
+define('DIR_CACHE', 'C:\wamp\www\opencart\upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\opencart\upload/download/');
+define('DIR_LOGS', 'C:\wamp\www\opencart\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart_1.5.4.1');
+define('DB_PREFIX', '');
+?>
