771e6aafca23ca241219369d35676e8207842bd3
Lev Ananikyan <lev.ananikyan@gmail.com>
2013-07-16 16:44:50 +0400

    vqmod added

---
+++ upload/vqmod/vqcache/vq2-admin_language_english_catalog_manufacturer.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']      = 'Manufacturer';
+
+// Text
+$_['text_success']       = 'Success: You have modified manufacturers!';
+$_['text_default']       = 'Default';
+$_['text_image_manager'] = 'Image Manager';
+$_['text_browse']        = 'Browse';
+$_['text_clear']         = 'Clear';
+$_['text_percent']       = 'Percentage';
+$_['text_amount']        = 'Fixed Amount';
+
+// Column
+$_['column_name']        = 'Manufacturer Name';
+$_['column_sort_order']  = 'Sort Order';
+$_['column_action']      = 'Action';
+
+// Entry
+$_['entry_name']         = 'Manufacturer Name:';
+$_['entry_store']        = 'Stores:';
+$_['entry_keyword']      = 'SEO Keyword:<br /><span class="help">Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.</span>';
+$_['entry_image']        = 'Image:';
+$_['entry_meta_description']= 'Meta Tag Description:';
+$_['entry_meta_keywords']= 'Meta Tag Keywords:';
+$_['entry_description']  = 'Description:';
+$_['entry_page_title']   = 'Page Title:';
+$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_type']         = 'Type:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify manufacturers!';
+$_['error_name']         = 'Manufacturer Name must be between 3 and 64 characters!';
+$_['error_product']      = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
+?>
